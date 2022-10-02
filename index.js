808212a9323c1052bdb8c030280673f4ae4104c6
unknown <altamar133@gmail.com>
2022-10-03 00:52:13 +0300

    add markup, styling and form open-close scripting

---
+++ index.js
@@ -0,0 +1,9 @@
+document.getElementById('openForm').style.display = 'none';
+
+function openForm() {
+    document.getElementById('openForm').style.display = 'block';
+}
+
+function closeForm() {
+    document.getElementById('openForm').style.display = 'none';
+}
