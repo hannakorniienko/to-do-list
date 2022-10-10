ddfa62a909fe4ea6bd0f3b7423c55dacee50f3ff
unknown <altamar133@gmail.com>
2022-10-10 22:44:09 +0300

    added edit function, delete function, alert massages

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const form = document.querySelector("form");
-const list = document.querySelector("ul");
-let borderColor = ""
+const form = document.querySelector("form")
+const list = document.querySelector("ul")
+const title = document.querySelector("#title")
+const date = document.querySelector("#date")
+const status = document.querySelector("#status")
+const lists = document.getElementsByClassName("todo__item")
+let boderColor = ""
+let currentItem
+const itemsList = []
 
 const toggleForm = () => {
     document.querySelector("#addBtn").style.display = "inline"
@@ -9,37 +15,67 @@ const toggleForm = () => {
 }
 
 const addItem = () => {
-    const title = document.querySelector("#title").value
-    const date = document.querySelector("#date").value
-    const status = document.querySelector("#status").value
-    const listItem = document.createElement("li")
-    listItem.innerHTML = `<h4>${title}</h4> <p>Deadline: ${date}</p>`
-    listItem.classList.add("todo__item")
-    switchColor()
-    listItem.style.borderLeft = `4px solid ${borderColor}`
-    list.appendChild(listItem)
-    insertEditBtn(listItem, title, date, status)
-    resetForm()
+    if (itemsList.includes(title.value) === true) {
+        alert("Title should be unique!")
+        return;
+    } if (title.value === "") {
+        alert("Write the title!")
+        return;
+    } if (date.value === "") {
+        alert("Choose the deadline date!")
+        return;
+    } if (status.value === "") {
+        alert("Choose the status!")
+        return;
+    } else {
+        const listItem = createItem()
+        list.appendChild(listItem)
+        resetForm()
+    }
 }
 
-const insertEditBtn = (listItem, title, date, status) => {
+const createItem = () => {
+    const listItem = document.createElement("li")
+    listItem.innerHTML = `<h4>${title.value}</h4> <p>${date.value}</p>`
+    listItem.classList.add("todo__item")
+    switchColor()
+    listItem.style.borderLeft = `5px solid ${boderColor}`
+    insertEditBtn(listItem)
+    insertDeleteBtn(listItem)
+    listItem.id = `${title.value}+date.value+status.value`
+    itemsList.push(title.value)
+    return listItem
+}
+
+const insertEditBtn = (listItem) => {
     const editBtn = document.createElement("button")
     editBtn.innerText = "Edit"
     editBtn.addEventListener("click", () => {
         form.style.display = "block"
-        document.querySelector("#title").value = title
-        document.querySelector("#date").value = date
-        document.querySelector("#status").value = status
+        title.value = listItem.querySelector("h4").innerText
+        date.value = listItem.querySelector("p").innerText
+        currentItem = listItem
+        document.querySelector("#addBtn").style.display = "none"
+        document.querySelector("#saveBtn").style.display = "inline"
     })
-    document.querySelector("#addBtn").style.display = "none"
-    document.querySelector("#saveBtn").style.display = "inline"
-    form.appendChild(saveBtn)
     listItem.appendChild(editBtn)
 }
 
-/* const onEdit = () => {
-    
-}*/
+const insertDeleteBtn = (listItem) => {
+    const deleteBtn = document.createElement("button")
+    deleteBtn.innerText = "Delete"
+    listItem.appendChild(deleteBtn)
+    deleteBtn.addEventListener("click", () => {
+        listItem.remove()
+    })
+}
+
+const onEdit = () => {
+    const parent = currentItem.parentNode /*  li  --> ul*/
+    const editedItem = createItem()
+    parent.replaceChild(editedItem, currentItem)
+    resetForm()
+}
 
 const resetForm = () => {
     form.style.display = "none"
@@ -52,16 +88,16 @@ const switchColor = () => {
     const status = document.querySelector("#status").value
     switch (status) {
         case "done":
-            borderColor = "green"
+            boderColor = "green"
             break
         case "in progress":
-            borderColor = "orange"
+            boderColor = "orange"
             break
         case "not started":
-            borderColor = "red"
+            boderColor = "red"
             break
         default:
-            borderColor = "white"
+            boderColor = "white"
     }
 }
 
